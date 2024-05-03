c222653a1fa8d1bdf3592bc96c28ec1a9acbd040
runehille <rune.hille@gmail.com>
2024-05-03 20:47:02 +0200

    Add validators to Project entity, and migrate to db

--- IssueOtter/Core/Entities/Project.cs
+++ IssueOtter/Core/Entities/Project.cs
@@ -1,27 +1,40 @@
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IssueOtter.Core.Entities;
 
-[Table("Project")]
 public class Project
 {
   [Required]
   public int Id { get; set; }
+
   [Required]
+  [MinLength(1)]
+  [MaxLength(10)]
   public string Key { get; set; } = string.Empty;
+
   [Required]
+  [MinLength(1)]
+  [MaxLength(50)]
   public string Title { get; set; } = string.Empty;
+
   public string Description { get; set; } = string.Empty;
+
   [Required]
   public int AdminId { get; set; }
+
   public User? Admin { get; set; }
+
   [Required]
   public DateTime CreatedOn { get; set; } = DateTime.Now;
+
   [Required]
   public int CreatedById { get; set; }
+
   public User? CreatedBy { get; set; }
+
   public List<Issue> Issues { get; set; } = [];
+
   public int IssueCount { get; set; } = 0;
+
   public bool IsDeleted { get; set; }
 }
---
+++ IssueOtter/Infrastructure/Migrations/20240503184010_AddValidatorsToProject.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddValidatorsToProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Project",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "Project",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Project",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "Project",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldMaxLength: 10)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
